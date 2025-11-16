2be3215c577da390e146ae2a8c8598e4d62026eb
tenor-polinomics <tensor.polinomics@gmail.com>
2025-11-16 23:26:07 +0000

    Updated spaCy learning

---
+++ nlp_spacy/py2.py
@@ -0,0 +1,17 @@
+from spacy.language import Language
+
+
+@Language.component("set_custom_boundaries")
+def set_custom_boundaries(doc):
+    for token in doc[:-1]:
+        if token.text == "...":
+            doc[token.i + 1].is_sent_start = True
+    return doc
+
+
+custom_nlp = spacy.load("en_core_web_sm")
+custom_nlp.add_pipe("set_custom_boundaries", before="parser")
+custom_ellipsis_doc = custom_nlp(ellipsis_text)
+custom_ellipsis_sentences = list(custom_ellipsis_doc.sents)
+for sentence in custom_ellipsis_sentences:
+    print(sentence)
